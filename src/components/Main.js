d80c671c0e84481719066f899727ccd060c9c423
thoraya elsharawy <thorayaelsharawy@gmail.com>
2024-06-27 00:01:09 +0300

    Add About section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,11 @@
+import About from "./About";
 import Menu from "./Menu";
 
 export default function Main() {
     return (
         <main>
             <Menu />
+            <About />
         </main>
     )
 }

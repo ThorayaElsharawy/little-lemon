6d2cbb0ffa95ac7454f078143095319462c018d7
thoraya elsharawy <thorayaelsharawy@gmail.com>
2024-06-27 12:57:38 +0300

    Add Testimonials section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import About from "./About";
 import Menu from "./Menu";
+import Testimonials from "./Testimonials";
 
 export default function Main() {
     return (
         <main>
             <Menu />
+            <Testimonials />
             <About />
         </main>
     )
